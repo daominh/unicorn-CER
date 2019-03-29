20865aafe0b95228453aa5a089b335eafb946151
Dao Minh <daoleminh0008@gmail.com>
2019-03-29 17:41:43 +0700

    Updates

---
+++ precache-manifest.d4bc7d8227bb52e3f780d34804a59025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d2e3ab5c06507ff4044",
+    "url": "/unicorn-CER/static/js/runtime~main.a4743196.js"
+  },
+  {
+    "revision": "138d451af94437f0c0eb",
+    "url": "/unicorn-CER/static/js/main.bf3fa965.chunk.js"
+  },
+  {
+    "revision": "240d72c60b94ce0c98ef",
+    "url": "/unicorn-CER/static/js/2.e5125876.chunk.js"
+  },
+  {
+    "revision": "138d451af94437f0c0eb",
+    "url": "/unicorn-CER/static/css/main.6b458ed8.chunk.css"
+  },
+  {
+    "revision": "240d72c60b94ce0c98ef",
+    "url": "/unicorn-CER/static/css/2.9a84818c.chunk.css"
+  },
+  {
+    "revision": "41c1fdcb2e5b91b650f2c4f80752d809",
+    "url": "/unicorn-CER/index.html"
+  }
+];
